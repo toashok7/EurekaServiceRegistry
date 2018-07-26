a65272a6008e6b6bd27b6538a83740567e6a1041
Cloud User <ec2-user@ip-172-31-32-48.us-east-2.compute.internal>
2018-07-26 18:03:48 +0000

    Eureka Service

---
+++ src/test/java/eurekaClient/EurekaTest1ApplicationTests.java
@@ -0,0 +1,16 @@
+package eurekaClient;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EurekaTest1ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
